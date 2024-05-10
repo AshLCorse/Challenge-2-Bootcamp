c9adc7dc20352e6bb1cb5806656d8eb40b69a768
AshLCorse <131395652+AshLCorse@users.noreply.github.com>
2024-05-09 20:26:59 -0400

    Tried some JS, edited CSS and HTML
    Tried some JavaScript, edited CSS and HTML to more match the acceptance criteria.

--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,6 @@
-var h1_list = document.querySelectorAll('h1');
-let arrayList = Array.from(h1_list);
-let index = arrayList.findIndex(e => e.innerText == h1_Title);
+var li_list = document.querySelectorAll('li');
+let arrayList = Array.from(li_list);
+let index = arrayList.findIndex(e => e.innerText == li_Title);
 
 if (index >-1){
     arrayList[index].scrollIntoView({ behavior: 'smooth', block: 'start', inline: 'start'});
